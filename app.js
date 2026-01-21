3c05d928446cf5daa972f9d015b140ecf8fc8258
Isabella <digital.media.GitHub@gmail.com>
2026-01-21 16:40:30 -0600

    First commit

---
+++ app.js
@@ -0,0 +1,9 @@
+// Console Message - This will appear in the browser console
+console.log("Hello Professor! This is my console message for the assignment.");
+console.log("Assignment 1.6");
+
+// document.write - This will display information on the HTML page
+document.write("<h1>JavaScript Assignment</h1>");
+document.write("<p>This content was added to the page using document.write()</p>");
+document.write("<p>April Sharer</p>");
+document.write("<p>Date: " + new Date().toLocaleDateString() + "</p>");
