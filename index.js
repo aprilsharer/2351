30a7884f569b641348b03ea296705bc07fd8b87a
Isabella <digital.media.GitHub@gmail.com>
2026-01-23 09:01:32 -0600

    April Sharer edits

---
+++ index.js
@@ -0,0 +1,31 @@
+let globalCounter = 100;
+let calculationResult = globalCounter * 2 + 15;
+
+function demonstrateScopes() {
+    console.log("Starting scope demonstration");
+    console.log(globalCounter);
+    
+    let mathExample = globalCounter / 5;
+    console.log(mathExample);
+    
+    if (true) {
+        let blockLevel = 42;
+        console.log(blockLevel);
+    }
+    
+    let userName = "April";
+    console.log(userName);
+    
+    const userProfile = {
+        name: "May",
+        age: 34,
+        city: "Austin"
+    };
+    
+    console.log(userProfile);
+    console.log(userProfile.name);
+}
+
+demonstrateScopes();
+
+console.log("Global variable accessible here too:")
